450a0f6737c6c59078efc372ae50d2ac1d413063
darshanraphal <126935481+darshanraphal@users.noreply.github.com>
2023-03-04 10:12:40 +0530

    Add files via upload
    ============== RESTART: C:/Users/student/greatest of 3 numbers.py ==============
    num1:
    24
    num2:
    56
    num3:
    32
    num2 is greater:

---
+++ greatest of 3 numbers.py
@@ -0,0 +1,10 @@
+num1=int(input("num1:\n"))
+num2=int(input("num2:\n"))
+num3=int(input("num3:\n"))
+max=0
+if num1>=num2 and num1>=num3:
+    print("num1 is greater:\n" )
+elif num2>=num1 and num2>=num3:
+    print("num2 is greater:\n")
+else:
+    print('num3 is greater:\n')
