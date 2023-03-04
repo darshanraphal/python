c2ccf95dd5693c3bab42d63502e581ecc8c7bc0b
darshanraphal <126935481+darshanraphal@users.noreply.github.com>
2023-03-04 11:53:45 +0530

    Add files via upload
    ================== RESTART: C:/Users/student/average marks.py ==================
    enter the marks of sub1:
    92
    enter the marks of sub2:
    90
    enter the marks of sub3:
    86
    enter the marks of sub4:
    73
    enter the marks of sub5:
    99
    average marks 88.0

---
+++ average marks.py
@@ -0,0 +1,7 @@
+a=float(input("enter the marks of sub1:\n"))
+b=float(input("enter the marks of sub2:\n"))
+c=float(input("enter the marks of sub3:\n"))
+d=float(input("enter the marks of sub4:\n"))
+e=float(input("enter the marks of sub5:\n"))
+total=a+b+c+d+e
+print("average marks",total/5 )
